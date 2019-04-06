419f7c38f2c37d73abb2fbb704f2e48a8f9db190
Vaselek <burzhuas@gmail.com>
2019-04-06 23:21:59 +0600

    Finish configuration

---
+++ server.js
@@ -0,0 +1,27 @@
+// const Link = require('./models/Link');
+// const links = require('./app/links');
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const config = require('./config');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static('public'));
+app.use(cors());
+
+const albums = require('./app/albums');
+const artists = require('./app/artists');
+const tracks = require('./app/tracks');
+
+const port = 8000;
+
+mongoose.connect(config.dbUrl, config.mongoOptions).then(() => {
+    app.use('/albums', albums);
+    app.use('/artists', artists);
+    app.use('/tracks', tracks);
+    app.listen(port, () => {
+        console.log(`Server started on ${port} port`)
+    })
+})
